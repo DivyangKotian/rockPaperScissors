587fc9420b5027ce8f8fcd3c0022ea84b7675b24
Divyang Kotian <kotian.divyang@gmail.com>
2024-05-03 15:37:06 -0400

    new changes to add GUI, incomplete

--- logic.js
+++ logic.js
@@ -1,43 +1,66 @@
 const choices=["rock","paper","scissor"];
+
+
 function getRandomChoice (choices){                      //func for random choice by comp
     return choices[Math.floor(choices.length*Math.random())];
 }
 
+let playerSelection;
+
+const btnRock = document.querySelector(`#rock`);
+const btnPaper = document.querySelector(`#paper`);
+const btnScissor = document.querySelector(`#scissor`);
+
+console.log(btnRock);
+console.log(btnPaper);
+console.log(btnScissor);
+
+btnRock.addEventListener(`click`, (e)=>{
+     playerSelection= "rock";
+     console.log(playerSelection);
+});
+
+btnPaper.addEventListener(`click`, (e)=>{
+     playerSelection= "paper";
+     console.log(playerSelection);
+});
+
+btnScissor.addEventListener(`click`, (e)=>{
+     playerSelection= "scissor";
+     console.log(playerSelection);
+});
+
+
 /*let computerSelection= getRandomChoice(choices);              // store computer random choice
 console.log(`computer chose ${computerSelection}`);              commented out and used later in playGame() func*/
 
-function userChoice (choices){                                    // converting player choice to lower, so we avoid any errors concerning case sensitivity
-    let playerChoice= prompt("Enter your weapon of mass destruction",);
-    let lower= playerChoice.toLowerCase();
-    return lower;
-}
 /*let playerSelection=userChoice(choices);                           // store user choice for later use
 console.log(`player chose ${playerSelection}`);                      playerSelection and computerSelection declared in the playGame() func as they arent needed until playOneRound is called */
 
-function playOneRound(computerSelection, playerSelection){              // intial code is only for one round of the game
+/*function playOneRound(computerSelection, playerSelection){              // intial code is only for one round of the game
     if (playerSelection!="paper"&&playerSelection!="rock"&&playerSelection!="scissor"){               // to avoid random choices apart from RPK
         return("Please choose either rock, paper or scissor to continue");             
     }
-
+    
     else if(computerSelection==playerSelection){                        //tie condition
         return("the round ends in a tie, try again");
     }
     else if (playerSelection=="rock"){                                 // adding all possible events
         if(computerSelection=="paper")
-            return ("you lose, paper beats rock");
-        else if(computerSelection="scissor")
-            return ("you win, rock beats scissor");
+        return ("you lose, paper beats rock");
+    else if(computerSelection="scissor")
+    return ("you win, rock beats scissor");
     }
     else if( playerSelection=="paper"){
             if(computerSelection=="scissor")
-                return (" you lose, scissor beats paper");
-             else if(computerSelection=="rock")
+            return (" you lose, scissor beats paper");
+        else if(computerSelection=="rock")
                  return ("you win, paper beats rock");
-    }
-    else if (playerSelection=="scissor"){
-        if(computerSelection=="rock")
-            return(" you lose, rock beats scissor");
-        if(computerSelection=="paper")
+            }
+            else if (playerSelection=="scissor"){
+                if(computerSelection=="rock")
+                return(" you lose, rock beats scissor");
+            if(computerSelection=="paper")
             return("you win, scissor beats rock");
     }
 }
@@ -48,33 +71,44 @@ function playGame(){                    //5 rounds function
     let round;
     let playerScore=0;
     let compScore=0;
-
+    
     for (round=1; round<6; round++ )
-        { 
-            console.log(`round ${round}`)
-            let computerSelection=getRandomChoice(choices);                  // storing the random generated choice by computer
+    { 
+        console.log(`round ${round}`)
+        let computerSelection=getRandomChoice(choices);                  // storing the random generated choice by computer
             console.log(`computer chose ${computerSelection}`);
             let playerSelection=userChoice(choices);                           // storing the player choice
             console.log(`player chose ${playerSelection}`);                         
             playOneRound(computerSelection, playerSelection);
             let result= playOneRound(computerSelection,playerSelection);
-        console.log(result);                                                    // round display counter
-        if(result.includes("win"))
-                 playerScore++;
+            console.log(result);                                                    // round display counter
+            if(result.includes("win"))
+            playerScore++;
         if( result.includes("lose"))
                  compScore++;
         if( result.includes("tie")|| result.includes("Please")){                // replaying round in case of tie
                  round--;
         }
                
-             console.log(`current score is player-${playerScore} and computer- ${compScore}`)       // basic score board
-        }
+        console.log(`current score is player-${playerScore} and computer- ${compScore}`)       // basic score board
+    }
     if (playerScore>compScore)                                                      // final winner 
-        console.log('player wins');
+    console.log('player wins');
     else
-        comsole.log('computer wins');
-        }
-    playGame();
-    
+    comsole.log('computer wins');
+}
+//playGame();
+*/
 
 
+
+
+
+
+
+
+/*function userChoice (choices){                                    // converting player choice to lower, so we avoid any errors concerning case sensitivity
+    let playerChoice= prompt("Enter your weapon of mass destruction",);
+    let lower= playerChoice.toLowerCase();
+    return lower;
+} */
